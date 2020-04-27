be17f01e8bc4553dd9d944e448ef08cf1f6eb31c
Vee Zhang <veezhang@126.com>
2020-04-27 22:44:53 +0800

    Prototype 原型模式

--- creational/prototype/prototype.go
+++ creational/prototype/prototype.go
@@ -1,1 +1,36 @@
 package prototype
+
+// 摘抄自 go 源码 ： golang/go/src/net/http/header.go
+// 这里做了简化，主要体现 原型模式
+
+// 其它的kubernetes 也使用了原型模式
+// github/kubernetes/kubernetes/pkg/kubelet/cm/cpuset/cpuset.go
+// func (s CPUSet) Clone() CPUSet { ... }
+
+type Header map[string]string
+
+// Set sets the value of key
+func (h Header) Set(key, value string) {
+	h[key] = value
+}
+
+// Set returns the value of key
+func (h Header) Get(key string) string {
+	if v, ok := h[key]; ok {
+		return v
+	}
+	return ""
+}
+
+func (h Header) Clone() Header {
+	if h == nil {
+		return nil
+	}
+
+	h2 := make(Header, len(h))
+	for k, v := range h {
+		h2[k] = v
+	}
+
+	return h2
+}
--- creational/prototype/prototype_test.go
+++ creational/prototype/prototype_test.go
@@ -1,1 +1,35 @@
 package prototype
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestPrototype(t *testing.T) {
+	h1 := make(Header, 0)
+
+	h1.Set("key1", "value1")
+	h1.Set("key2", "value2")
+	h1.Set("key3", "value3")
+	h1.Set("key4", "value4")
+
+	h2 := h1.Clone()
+
+	diff := cmp.Diff(h1, h2)
+	if diff != "" {
+		t.Errorf(diff)
+	}
+
+	h1.Set("key2", "value2-changed")
+	h1k := h1.Get("key2")
+	if h1k != "value2-changed" {
+		t.Errorf("h1 set key2 = \"value2-changed\" failed, get \"%s\".", h1k)
+	}
+
+	h2k := h2.Get("key2")
+	if h2k != "value2" {
+		t.Errorf("h2 get key2 \"%s\".", h2k)
+	}
+
+}
